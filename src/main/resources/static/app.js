32e132191228116ddbb76e95c98d25c27229573a
Kristofer <kris@youngers.org>
2024-04-22 13:15:35 -0400

    checkpoint

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,54 @@
+const API_URL = `http://localhost:8080`;
+
+function fetchData() {
+  fetch(`${API_URL}/api/tickets`)
+    .then(res => {
+      //console.log("res is ", Object.prototype.toString.call(res));
+      return res.json();
+    })
+    .then(data => {
+      show(data);
+    })
+    .catch(error => {
+      console.log(`Error Fetching data : ${error}`);
+      document.getElementById('posts').innerHTML = 'Error Loading Data';
+    });
+}
+
+// takes a UNIX integer date, and produces a prettier human string
+function dateOf(date) {
+  const milliseconds = date * 1000; // 1575909015000
+  const dateObject = new Date(milliseconds);
+  const humanDateFormat = dateObject.toLocaleString(); //2019-12-9 10:30:15
+  return humanDateFormat;
+}
+
+function show(data) {
+  // the data parameter will be a JS array of JS objects
+  // this uses a combination of "HTML building" DOM methods (the document createElements) and
+  // simple string interpolation (see the 'a' tag on title)
+  // both are valid ways of building the html.
+  const ul = document.getElementById('posts');
+  const list = document.createDocumentFragment();
+
+  data.map(function (post) {
+    console.log('Story:', post);
+    let li = document.createElement('li');
+    let title = document.createElement('h3');
+    let body = document.createElement('p');
+    let by = document.createElement('p');
+    title.innerHTML = `<a href="/api/tickets/${post.id}">${post.title}</a>`;
+    body.innerHTML = `${post.description}`;
+    //let postedTime = dateOf(post.time)
+    by.innerHTML = `${post.date} - ${post.reportedBy}`;
+
+    li.appendChild(title);
+    li.appendChild(body);
+    li.appendChild(by);
+    list.appendChild(li);
+  });
+
+  ul.appendChild(list);
+}
+
+fetchData();
